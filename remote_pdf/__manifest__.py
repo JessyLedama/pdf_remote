689e68537fb8241902e327970562e27e5dbc1000
Jayliste <jaylisteledama94@gmail.com>
2023-01-07 19:13:28 +0300

    [UPD] updated manifest to include directories view.

--- remote_pdf/__manifest__.py
+++ remote_pdf/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Remote PDF',
+    'name': 'Remote Tool',
     'summary': """copy file from local folder to remote folder, and vice versa.""",
     'description': """
         When you download a file to a specified folder, it is copied to a remote location, and can be copied from remote location back to a specified local directory.
@@ -11,6 +11,7 @@
     'depends': ['web', 'base'],
     'data': [
         'views/ir_actions_report.xml',
+        'views/directories.xml',
     ],
     'installable': True,
     'auto_install': False,
